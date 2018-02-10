3c68ef031f51b80490393950e400773305a843d0
Petr Benes <xbenes@centrum.cz>
2018-02-10 10:10:43 +0100

    Step 4: date picker

--- src/sun.js
+++ src/sun.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
 
 export default class extends React.Component {
     constructor(...args) {
@@ -14,11 +17,24 @@ export default class extends React.Component {
         });
     }
 
+    handleDayClick(date) {
+        this.setState({
+            date: date
+        });
+    }
+
     render() {
         return (
             <div>
                 <input onChange={this.textChange} />
                 <div>{this.state.text}</div>
+
+                <div>{String(this.state.date)}</div>
+                <DayPicker
+                    selectedDays={[this.state.date]}
+                    onDayClick={this.handleDayClick.bind(this)}
+                />
+
             </div>
         );
     }
