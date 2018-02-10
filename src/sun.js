4dfdb97eafec2fb36f9f192d9e85aa38202bce2e
Petr Benes <xbenes@centrum.cz>
2018-02-10 10:10:43 +0100

    Step 3: onChange handler

--- src/sun.js
+++ src/sun.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 export default class extends React.Component {
+    constructor(...args) {
+        super(...args);
+        this.state = {};
+
+        this.textChange = this.textChange.bind(this);
+    }
+
+    textChange(e) {
+        this.setState({
+            text: e.target.value
+        });
+    }
+
     render() {
-        return <div>Sun</div>;
+        return (
+            <div>
+                <input onChange={this.textChange} />
+                <div>{this.state.text}</div>
+            </div>
+        );
     }
 }
