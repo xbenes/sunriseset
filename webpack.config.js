7dfb63054baa41b383acbabce00155fe92541e32
Petr Benes <xbenes@centrum.cz>
2018-02-10 10:10:43 +0100

    Step 0: basic setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/app.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }, {
+            test: /\.html$/,
+            use: [{
+                loader: 'html-loader'
+            }]
+        }, {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+};
+
