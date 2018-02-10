c0c80eb0c05aa822a34cb68345c798cf575b59bf
Petr Benes <xbenes@centrum.cz>
2018-02-10 10:10:43 +0100

    Step 6: compute sunrise/sunset

--- src/sun.js
+++ src/sun.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import SolarCalc from 'solar-calc';
 
 export default class extends React.Component {
     constructor(...args) {
         super(...args);
-        this.state = {};
+        this.state = {
+            lat: 49.195,
+            lon: 16.606,
+            locName: 'Brno'
+        };
     }
 
     handleDayClick(date) {
+        const calc = new SolarCalc(date, this.state.lat, this.state.lon);
         this.setState({
-            date: date
-        });
+            date,
+            sunrise: calc.sunrise,
+            sunset: calc.sunset
+        })
     }
 
     render() {
         return (
             <div>
-                <div>{String(this.state.date)}</div>
                 <DayPicker
                     selectedDays={[this.state.date]}
                     onDayClick={this.handleDayClick.bind(this)}
                 />
+                <div>{String(this.state.date)}</div>
+                <div>{String(this.state.sunrise)}</div>
+                <div>{String(this.state.sunset)}</div>
             </div>
         );
     }
