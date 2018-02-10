9b5331a649c3a9b169dabbd500391782e4be7efc
Petr Benes <xbenes@centrum.cz>
2018-02-10 10:10:43 +0100

    Step 5: removed onclick handler

--- src/sun.js
+++ src/sun.js
@@ -2,19 +2,10 @@ import React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-
 export default class extends React.Component {
     constructor(...args) {
         super(...args);
         this.state = {};
-
-        this.textChange = this.textChange.bind(this);
-    }
-
-    textChange(e) {
-        this.setState({
-            text: e.target.value
-        });
     }
 
     handleDayClick(date) {
@@ -26,15 +17,11 @@ export default class extends React.Component {
     render() {
         return (
             <div>
-                <input onChange={this.textChange} />
-                <div>{this.state.text}</div>
-
                 <div>{String(this.state.date)}</div>
                 <DayPicker
                     selectedDays={[this.state.date]}
                     onDayClick={this.handleDayClick.bind(this)}
                 />
-
             </div>
         );
     }
