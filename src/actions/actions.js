fd3a7cc03b3902916a5472fc1e4146268a3eec46
petr <xbenes@centrum.cz>
2018-11-14 18:50:12 +0100

    Step 13.5: Call api on date change (outside)

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,25 @@
+import { formatDate } from '../datetime';
+
 export function changeDate(date) {
-    return {
-        type: 'CHANGE_DATE',
-        date
+    return (dispatch, getState) => {
+        dispatch({
+            type: 'CHANGE_DATE',
+            date
+        });
+
+        const lat = 49.195;
+        const lon = 16.606;
+        const locName = 'Brno';
+
+        const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=${formatDate(date)}&formatted=0`;
+        return fetch(url).then(response => {
+            return response.json();
+        }).then(response => {
+            dispatch({
+                type: 'UPDATE_SUNRISE_SUNSET',
+                sunrise: new Date(response.results.sunrise),
+                sunset: new Date(response.results.sunset)
+            });
+        });
     };
 }
