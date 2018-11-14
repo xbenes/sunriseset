ef1958d13cb537caebfdc68f87e188358e9f3ad0
petr <xbenes@centrum.cz>
2018-11-14 18:50:12 +0100

    Step 15.3 Recompute on location change

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,12 +1,8 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, select } from 'redux-saga/effects';
 import { updateSunriseSunset } from '../actions/actions';
 import { formatDate } from '../datetime';
 
-function getSunInfo(date) {
-    const lat = 49.195;
-    const lon = 16.606;
-    const locName = 'Brno';
-
+function getSunInfo(date, lat, lon) {
     const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=${formatDate(date)}&formatted=0`;
     return fetch(url).then(response => {
         return response.json();
@@ -17,12 +13,29 @@ function getSunInfo(date) {
 }
 
 function* changeDate(action) {
-    const info = yield call(getSunInfo, action.date);
+    const state = yield select();
+
+    const selected = state.location.location;
+    const latlon = state.location.locations[selected];
+
+    const info = yield call(getSunInfo, action.date, latlon.lat, latlon.lon);
+    yield put(updateSunriseSunset(info.sunrise, info.sunset));
+}
+
+function* changeLocation(action) {
+    const state = yield select();
+    const date = state.date;
+
+    const selected = action.location;
+    const latlon = state.location.locations[selected];
+
+    const info = yield call(getSunInfo, date, latlon.lat, latlon.lon);
     yield put(updateSunriseSunset(info.sunrise, info.sunset));
 }
 
 function* rootSaga() {
     yield takeLatest('CHANGE_DATE', changeDate);
+    yield takeLatest('CHANGE_LOCATION', changeLocation);
 }
 
 export default rootSaga;
